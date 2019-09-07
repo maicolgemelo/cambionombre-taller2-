a84b5b44634a4caa8c4b40f08dcf9dd6c1e9c730
michael escobar galeano <maicolgemelo@gmail.com>
2019-09-06 19:43:49 -0500

    programa que cambia el diseño del nombre ingresado

---
+++ Diseño_Nombre/Diseño_Nombre/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Diseño_Nombre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblnombrediseño = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.btnok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(281, 49);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(153, 24);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Diseño nombre";
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnombre.Location = new System.Drawing.Point(165, 156);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(163, 18);
+            this.lblnombre.TabIndex = 1;
+            this.lblnombre.Text = "ingrese su nombre : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(226, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Diseño :";
+            // 
+            // lblnombrediseño
+            // 
+            this.lblnombrediseño.AutoSize = true;
+            this.lblnombrediseño.Location = new System.Drawing.Point(331, 242);
+            this.lblnombrediseño.Name = "lblnombrediseño";
+            this.lblnombrediseño.Size = new System.Drawing.Size(84, 13);
+            this.lblnombrediseño.TabIndex = 3;
+            this.lblnombrediseño.Text = "nombre y diseño";
+            this.lblnombrediseño.Visible = false;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombre.Location = new System.Drawing.Point(334, 154);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 22);
+            this.txtnombre.TabIndex = 4;
+            // 
+            // btnok
+            // 
+            this.btnok.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnok.Location = new System.Drawing.Point(473, 150);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(75, 23);
+            this.btnok.TabIndex = 5;
+            this.btnok.Text = "OK";
+            this.btnok.UseVisualStyleBackColor = true;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnok);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.lblnombrediseño);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblnombrediseño;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Button btnok;
+    }
+}
+
---
+++ Diseño_Nombre/Diseño_Nombre/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diseño_Nombre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            string nombre = txtnombre.Text;
+
+            Font fo = txtnombre.Font;
+            lblnombrediseño.Font = new Font("Forte", emSize: 22, style: FontStyle.Bold);
+            lblnombrediseño.ForeColor = Color.Blue;
+            lblnombrediseño.Visible = true;
+
+            lblnombrediseño.Text = txtnombre.Text;
+
+        }
+    }
+}
